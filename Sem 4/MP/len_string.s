83a2675e5e3e1746dc10f3442aeac46572ca3307
Yadu <57983550+Yadu9238@users.noreply.github.com>
2020-05-29 09:01:19 +0530

    fixed error

--- Sem 4/MP/len_string.s
+++ Sem 4/MP/len_string.s
@@ -6,9 +6,10 @@ area	string1,code
 	adr r1,srcstr
 	mov r0,#0		
 repeat	ldrb r2,[r1],#1
-	add r0,r0,#1
 	cmp r2,#0
-	bne	repeat
+	beq go
+	add r0,r0,#1
+	bal repeat
 			
 go	b go
 srcstr	dcb	"this is source string",0
